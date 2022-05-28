31562e455562269dafc6e639d091a18eaf154ab0
ftm1337 <ftm1337@protonmail.com>
2022-05-28 06:53:15 -0400

    v1

--- src/hooks/useApproveCallback.ts
+++ src/hooks/useApproveCallback.ts
@@ -33,7 +33,7 @@ export function useApproveCallback(
   // check the current approval status
   const approvalState: ApprovalState = useMemo(() => {
     if (!amountToApprove || !spender) return ApprovalState.UNKNOWN
-    if (amountToApprove.currency === EXPANSE) return ApprovalState.APPROVED
+    if (amountToApprove.currency === ETHER) return ApprovalState.APPROVED
     // we might not have enough data to know whether or not we need to approve
     if (!currentAllowance) return ApprovalState.UNKNOWN
 
